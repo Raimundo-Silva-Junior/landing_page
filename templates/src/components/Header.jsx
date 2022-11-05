b7df6e8f2ec3a33bcca2fb03335ee78721494d7d
Lucas Duarte <lucs.rkt@gmail.com>
2022-11-05 13:03:28 -0300

    front: create header

---
+++ templates/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Logo da aplicação</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Sobre</li>
+          <li>Contatos</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
