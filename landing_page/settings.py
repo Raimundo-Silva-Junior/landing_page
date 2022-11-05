1e0dc6b9bc3937cf951dec56a1ed83b30ef34ae6
Raimundo Silva <raimundo.36@hotmail.com>
2022-11-05 12:49:04 -0300

    version 1.3

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'landing_page.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/build')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates/build')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
